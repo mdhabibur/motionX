12b7f128856fd7a81d45f2d5accaf109429252ca
Mohammad Ali <73034805+mdhabibur@users.noreply.github.com>
2024-11-05 19:28:28 +0600

    Delete frontend/src directory

--- frontend/src/components/analytics/AnalyticsHistoryBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AnalyticsHistoryBtn = ({title}) => {
-  return (
-    <button className='bg-gray-300 text-black  py-2 px-4 rounded-full text-xs hover:font-bold'>{title}</button>
-  )
-}
-
-export default AnalyticsHistoryBtn
